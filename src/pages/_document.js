7e233fc0163db0af963adbe6a447ba9dd6a3967a
MarkyyyyyyGG <clerigom7@gmail.com>
2024-07-08 10:34:36 +0800

    vercer fix 3

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-<script async src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>;
 
 export default function Document() {
   return (
